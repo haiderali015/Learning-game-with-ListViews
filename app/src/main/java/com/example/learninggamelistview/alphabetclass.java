d75d2593f443d22893e27061a451cbf1ef75f6b4
haider ali <ha347278@gmail.com>
2022-07-25 20:20:04 +0500

    new java class

---
+++ app/src/main/java/com/example/learninggamelistview/alphabetclass.java
@@ -0,0 +1,25 @@
+package com.example.learninggamelistview;
+
+public class alphabetclass {
+    String alphabetName;
+    String smallName;
+    String alphabetStandsfor;
+
+    public alphabetclass(String alphabetName, String smallName, String alphabetStandsfor) {
+        this.alphabetName = alphabetName;
+        this.smallName = smallName;
+        this.alphabetStandsfor = alphabetStandsfor;
+    }
+
+    public String getAlphabetName() {
+        return alphabetName;
+    }
+
+    public String getSmallName() {
+        return smallName;
+    }
+
+    public String getAlphabetStandsfor() {
+        return alphabetStandsfor;
+    }
+}
