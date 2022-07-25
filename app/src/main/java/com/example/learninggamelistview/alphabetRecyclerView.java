a46f5a8e84f4cb5dc6d65cf280a154f4453ed7b2
haider ali <ha347278@gmail.com>
2022-07-25 23:02:19 +0500

    layout inflater

--- app/src/main/java/com/example/learninggamelistview/alphabetRecyclerView.java
+++ app/src/main/java/com/example/learninggamelistview/alphabetRecyclerView.java
@@ -1,32 +1,54 @@
 package com.example.learninggamelistview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class alphabetRecyclerView extends RecyclerView.Adapter<alphabetRecyclerView.MyViewHolder>{
+    ArrayList<alphabetclass> alphabetclass;
+    Context context;
+    public alphabetRecyclerView (Context context, ArrayList<alphabetclass> alphabetclass){
+        this.context=context;
+        this.alphabetclass=alphabetclass;
+    }
+
+
+
+
     @NonNull
     @Override
     public alphabetRecyclerView.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater= LayoutInflater.from(context);
+        
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull alphabetRecyclerView.MyViewHolder holder, int position) {
-
+        holder.txt1.setText(alphabetclass.get(position).getAlphabetName());
+        holder.txt2.setText(alphabetclass.get(position).getSmallName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return alphabetclass.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
+        TextView txt1,txt2,txt3;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            txt1=itemView.findViewById(R.id.capital);
+            txt2=itemView.findViewById(R.id.small);
         }
     }
 }
