d06d122b79ee84f31a35483e882b4517f90d8e9d
haider ali <ha347278@gmail.com>
2022-07-27 02:12:48 +0500

    adding another activity

--- app/src/main/java/com/example/learninggamelistview/alphabetclass.java
+++ app/src/main/java/com/example/learninggamelistview/alphabetclass.java
@@ -4,17 +4,25 @@ public class alphabetclass {
     String alphabetName;
     String smallName;
     String alphabetStandsfor;
+    String picturename;
+    int image;
 
-    public alphabetclass(String alphabetName, String smallName, String alphabetStandsfor) {
+    public alphabetclass(String alphabetName, String smallName, String alphabetStandsfor,String picturename,int image;) {
         this.alphabetName = alphabetName;
         this.smallName = smallName;
         this.alphabetStandsfor = alphabetStandsfor;
+        this.picturename=picturename;
+        this.image=image;
     }
 
-    public alphabetclass(String alphabetName, String smallName) {
+    public alphabetclass(String alphabetName, String smallName,String picturename,int image;) {
         this.alphabetName = alphabetName;
         this.smallName = smallName;
         this.alphabetStandsfor = alphabetStandsfor;
+        this.picturename=picturename;
+        this.image=image;
+
+
     }
 
 
@@ -28,5 +36,10 @@ public class alphabetclass {
 
     public String getAlphabetStandsfor() {
         return alphabetStandsfor;
+    }
+    public String getPicturename(){return picturename;}
+
+    public int getImage() {
+        return 0;
     }
 }
--- app/src/main/java/com/example/learninggamelistview/secondA.java
+++ app/src/main/java/com/example/learninggamelistview/secondA.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.ArrayList;
@@ -28,15 +29,23 @@ public class secondA extends AppCompatActivity implements RecyclerViewInterface{
         String[] full_sentence = getResources().getStringArray(R.array.full_sentence);
         String[] one_letter_text = getResources().getStringArray(R.array.one_letter_text);
         String[] standsfor=getResources().getStringArray(R.array.standsfor);
+        int[] images=
+        String[] pictureName=
 
         for (int i =0; i<full_sentence.length; i++)
         {
-            alphabetclass.add(new alphabetclass(full_sentence[i],one_letter_text[i],standsfor[i]));
+            alphabetclass.add(new alphabetclass(full_sentence[i],one_letter_text[i],standsfor[i],
+                    ));
         }
     }
 
     @Override
     public void onItemclick(int position) {
+        Intent intent = new Intent(secondA.this,MainActivity2.class);
+        intent.putExtra("Name",alphabetclass.get(position).getPicturename());
+        intent.putExtra("Image",alphabetclass.get(position).getImage());
+        startActivity(intent);
+
 
     }
 }
